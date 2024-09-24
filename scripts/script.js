b615f90e715ae4d7dff4aa8eb829da766fbe6708
Samuel Esteban Rivera Mosquera <seriveramosq@gmail.com>
2024-09-23 22:38:06 -0500

    Update 0.6
    Style Gallery

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
-alert("reload")
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll(".gallery img");
+    const projectNumber = document.getElementById("project-number");
+    const projectTitle = document.getElementById("project-title");
+    const projectDescription = document.getElementById("project-description");
+
+    updateTextInfo({ number: "00", title: "CHOOSE", description: "A project." });
+
+    images.forEach((img) => {
+        img.addEventListener("click", () => {
+            images.forEach((img) => {
+                img.style.width = "0px";
+                img.style.opacity = ".9";
+                img.style.filter = "grayscale(90%)";
+            });
+
+            img.style.width = "25%";
+            img.style.opacity = "1";
+            img.style.filter = "contrast(125%)";
+
+            const info = JSON.parse(img.getAttribute("data-info"));
+            updateTextInfo(info);
+        });
+    });
+
+    function updateTextInfo(info) {
+        projectNumber.textContent = info.number;
+        projectTitle.textContent = info.title;
+        projectDescription.textContent = info.description;
+    }
+});
